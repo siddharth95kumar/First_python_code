d732cf8781ebe35ff3bfb6b2b8a8bae77b30324f
Siddharth <siddharth.kmr1995@gmail.com>
2021-12-18 16:32:20 +0530

    first

---
+++ Javascript/customvalidation.js
@@ -0,0 +1,21 @@
+function checkEmpty(info)
+{
+    if(info.length==0)
+        return false
+    else
+        return true
+}
+function checkPass(pass)
+{
+    if(pass.length<8)
+        return false
+    else
+         return true
+}
+function checkcombo(data)
+{
+    if(data=="default")
+         return false
+    else
+        return true
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# First_python_code" 
